c085da45a427cd716a2bc9cd07505d3b7d910538
Cristian Achille <blackdevil93@gmail.com>
2015-01-05 00:47:42 +0100

    E anche backtired ci lascia

--- bot/src/it/superamici/ircBot/plugins/quotemanager/QuoteListener.java
+++ bot/src/it/superamici/ircBot/plugins/quotemanager/QuoteListener.java
@@ -24,7 +24,9 @@ public class QuoteListener extends VariousMessageListenerAdapter {
     @Override
     public void onChannelMessage(ChannelPrivMsg aMsg) {
         if (aMsg.getText().contains("!addquote") && aMsg.getText().length() >= 10) {
-            if(aMsg.getSource().getNick().equals("pancreas") || aMsg.getSource().getNick().equals("pancraes") )
+            if(aMsg.getSource().getNick().equals("pancreas") ||
+               aMsg.getSource().getNick().equals("pancraes") ||
+               aMsg.getSource().getNick().equals("<Backtired"))
             {}
             else {
                 long n = quote.add(aMsg);
